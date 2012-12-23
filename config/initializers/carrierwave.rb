22203aa70bc40adf30e29983c569bf874bac959c
Pedro Belasco[MBP] <pbelasco@gmail.com>
2012-12-23 08:08:13 -0200

    cw

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
-    :provider               => 'AWS',       # required
-    :aws_access_key_id      => 'AKIAIDKW4H3R4H5QTRNQ',       # required
-    :aws_secret_access_key  => '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',       # required
-    :region                 => 'sa-east-1'  # optional, defaults to 'us-east-1'
+    :provider               => 'AWS',                   # required
+    :aws_access_key_id      => ENV['AWS_KEY'],          # required
+    :aws_secret_access_key  => ENV['AWS_SECRET'],       # required
+    :region                 => 'sa-east-1'              # optional, defaults to 'us-east-1'
   }
   config.fog_directory  = 'oh-site'                     # required
   config.fog_host       = 'https://s3-sa-east-1.amazonaws.com/oh-site'            # optional, defaults to nil
